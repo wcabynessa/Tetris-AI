d2ff4fe8c5f85a47f65a81bea925a21370f0a20a
Nguyen Quang Dung <nqdung14@gmail.com>
2017-04-01 21:09:30 +0800

    Update AdvanceState

--- src/AdvancedState.java
+++ src/AdvancedState.java
@@ -1,10 +1,20 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.IntStream;
+
 
 public class AdvancedState extends State {
 
     private static int aggregateHeight = 0;
     private static int numHoles = 0;
     private static int bumpiness = 0;
+    private static int randomSeed;
+    private Random rand;
+
+    public AdvancedState(long randomSeed) {
+        super();
+        rand = new Random(randomSeed);
+    }
     
     public int getAggregateHeight() {
         aggregateHeight = IntStream.of(top).sum();
@@ -31,6 +41,28 @@ public class AdvancedState extends State {
             bumpiness += Math.abs(top[i] - top[i + 1]);
         }
         return bumpiness;
+    }
+
+    @Override
+    protected int randomPiece() {
+        return (int)(rand.nextDouble() * N_PIECES);
+    }
+
+    public int[][] copy2DArray(int[][] arr) {
+        int[][] copy = new int[arr.length][arr[0].length];
+        for (int i = 0;  i < arr.length;  i++) {
+            copy[i] = Arrays.copyOf(arr[i], arr[i].length);
+        }
+        return copy;
+    }
+    
+
+    public AdvancedState clone() {
+        AdvancedState clonedState = new AdvancedState(randomSeed);
+        clonedState.field = copy2DArray(getField());
+        clonedState.top = Arrays.copyOf(getTop(), getTop().length);
+        clonedState.nextPiece = getNextPiece();
+        return clonedState;
     }
     
 }
