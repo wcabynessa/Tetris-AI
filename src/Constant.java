ca2bb21c10131617f9847bb5bd03c35acf54e7f1
Nguyen Quang Dung <nqdung14@gmail.com>
2017-04-02 05:00:06 +0800

    Add Feature highest column

--- src/Constant.java
+++ src/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 	public static final int POPULATION_SIZE = 100;
 	public static final int PERCENTAGE_CROSS_OVER = 10;
 	public static final int PERCENTAGE_MUTATION = 10;
-	public static final int NUMB_FEATURES = 4;
+	public static final int NUMB_FEATURES = 5;
 	public static final int NUMB_ITERATIONS = 20000;
 	public static final int NUMB_GAMES_PER_UPDATE = 5;
 
@@ -13,6 +13,7 @@ public class Constant {
     public static final int ROW_ELIMINATED = 1;
     public static final int NUM_HOLES = 2;
     public static final int BUMBINESS = 3;
+    public static final int HIGHEST_COL = 4;
 
     // Random seeds must match number of games per update above ^
     public static final long[] SEEDS = {
