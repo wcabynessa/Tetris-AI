2b65debeeb75700e0cb96bb03769379f8ae3b924
Nguyen Quang Dung <nqdung14@gmail.com>
2017-04-02 03:23:49 +0800

    Add scripts to test weights

---
+++ src/TestWeights.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class TestWeights {
+
+    public static double[] weights = new double[Constant.NUMB_FEATURES];
+
+    private static double computeUtility(AdvancedState oldState, AdvancedState newState) {
+        int aggregateHeight = newState.getAggregateHeight();
+        int rowsCleared = newState.getRowsCleared() - oldState.getRowsCleared();
+        int numHoles = newState.getNumHoles();
+        int bumpiness = newState.getBumpiness();
+
+        double utility = weights[Constant.AGGREGATE_HEIGHT] * aggregateHeight
+                        + weights[Constant.ROW_ELIMINATED] * rowsCleared
+                        + weights[Constant.NUM_HOLES] * numHoles
+                        + weights[Constant.BUMBINESS] * bumpiness;
+        return utility;
+    }
+
+	//implement this function to have a working system
+	private static int pickMove(AdvancedState state, int[][] legalMoves) {
+        double bestUtility = -Double.MAX_VALUE;
+        int bestMove = 0;
+
+        System.out.println("Current state: " + state.getAggregateHeight() + " | " +
+                                                 state.getRowsCleared() + "|" +
+                                                 state.getNumHoles() + "|" +
+                                                 state.getBumpiness() + " ---> ");
+        System.out.println("Start picking moves");
+        for (int move = 0;  move < legalMoves.length;  move++) {
+            AdvancedState cs = state.clone();
+            cs.makeMove(move);
+
+            double utility = computeUtility(state, cs);
+            //System.out.println("Possible utility: " + utility);
+            System.out.println("Possible move: " + cs.getAggregateHeight() + " | " +
+                                                    (cs.getRowsCleared() - state.getRowsCleared()) + "|" +
+                                                    cs.getNumHoles() + "|" +
+                                                    cs.getBumpiness() + " ---> " + utility);
+            if (utility > bestUtility) {
+                bestUtility = utility;
+                bestMove = move;
+            }
+        }
+        System.out.println("Found best move: " + legalMoves[bestMove][0] + " - " + legalMoves[bestMove][1]);
+        return bestMove;
+	}
+	
+
+	public static void main(String[] args) {
+        for (int i = 0;  i < args.length;  i++) {
+            weights[i] = Double.parseDouble(args[i]);
+        }
+		AdvancedState s = new AdvancedState((new Random()).nextLong());
+        State.initializeLegalMoves();
+		new TFrame(s);
+        Scanner sc = new Scanner(System.in);
+		while(!s.hasLost()) {
+			s.makeMove(pickMove(s,s.legalMoves()));
+			s.draw();
+			s.drawNext(0,0);
+			try {
+                //sc.next();
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+            /*System.out.println("Height: " + s.getAggregateHeight());
+            System.out.println("Rows cleared: " + s.getRowsCleared());
+            System.out.println("Holes: " + s.getNumHoles());
+            System.out.println("Bumbiness: " + s.getBumpiness());*/
+		}
+		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
+	}
+	
+}
