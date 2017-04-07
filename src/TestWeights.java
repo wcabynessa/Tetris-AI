f17afbf76de2444eb086d1b790c0b274af491517
Nguyen Quang Dung <nqdung14@gmail.com>
2017-04-08 07:39:24 +0800

    Add look-ahead

--- src/TestWeights.java
+++ src/TestWeights.java
@@ -8,15 +8,15 @@ public class TestWeights {
 
     public static double[] weights = new double[Constant.NUMB_FEATURES];
 
-    private static double computeUtility(AdvancedState oldState, AdvancedState newState, int move) {
+    private static double computeUtility(AdvancedState oldState, AdvancedState newState, int move, int rowsCleared) {
         // Landing height is the height of the column where the piece is put
         int piece = oldState.getNextPiece();
-        int orient = oldState.legalMoves()[move][State.ORIENT];
-        int slot = oldState.legalMoves()[move][State.SLOT];
+        int orient = oldState.legalMoves()[move][AdvancedState.ORIENT];
+        int slot = oldState.legalMoves()[move][AdvancedState.SLOT];
         int pieceWidth = oldState.pWidth[piece][orient];
         int landingHeight = Utility.arrayMax(newState.top, slot, slot + pieceWidth);
 
-        int rowsEliminated = newState.getRowsCleared() - oldState.getRowsCleared();
+        int rowsEliminated = newState.getRowsCleared() - oldState.getRowsCleared() + rowsCleared;
         int rowTransitions = newState.getBumpiness();
         int colTransitions = newState.getColTransitions();
         int numHoles = newState.getNumHoles();
@@ -28,23 +28,48 @@ public class TestWeights {
                         + weights[Constant.COL_TRANSITIONS] * colTransitions
                         + weights[Constant.NUM_HOLES] * numHoles
                         + weights[Constant.WELL_SUM] * wellSum;
-
-        //newState.printTop();
-        //newState.printField();
-        System.out.println("Score: " + newState.getRowsCleared());
-        System.out.println("Possible move: " +
-                            landingHeight + " | " +
-                            rowsEliminated + " | " +
-                            rowTransitions + " | " +
-                            colTransitions + " | " +
-                            numHoles + " | " +
-                            wellSum + " | " + utility);
+ 
         return utility;
+    }
+
+    private static Utility.IntDoublePair computeUtilityWithLookAhead(AdvancedState oldState, AdvancedState newState) {
+        int rowsCleared = newState.getRowsCleared() - oldState.getRowsCleared();
+        Utility.IntDoublePair totalUtility = new Utility.IntDoublePair(0, 0);
+        // Look-ahead try all possible move
+        for (int i = 0;  i < AdvancedState.N_PIECES;  i++) {
+            newState.setNextPiece(i);
+
+            // Find best move if the next piece is i
+            double tempBestUtility = -Double.MAX_VALUE;
+            int tempBestMove = -1;
+            for (int move = 0;  move < newState.legalMoves().length;  move++) {
+                AdvancedState lookAheadState = newState.clone();
+                lookAheadState.makeMove(move);
+                if (lookAheadState.hasLost()) {
+                    continue;
+                }
+
+                double utility = computeUtility(newState, lookAheadState, move, rowsCleared);
+                if (utility > tempBestUtility) {
+                    tempBestUtility = utility;
+                    tempBestMove = move;
+                }
+            }
+
+            // First value is the number of deadends
+            // Second value is the total utility of non-dead moves
+            if (tempBestMove == -1) {
+                totalUtility.first--;
+            } else {
+                totalUtility.second += tempBestUtility;
+            }
+        }
+        return totalUtility;
     }
 
 	//implement this function to have a working system
 	private static int pickMove(AdvancedState state, int[][] legalMoves) {
-        double bestUtility = -Double.MAX_VALUE;
+        Utility.IntDoublePair bestUtility = new Utility.IntDoublePair(-Integer.MAX_VALUE, -Double.MAX_VALUE);
         int bestMove = 0;
 
         System.out.println("rows: " + state.getRowsCleared());
@@ -52,18 +77,19 @@ public class TestWeights {
         for (int move = 0;  move < legalMoves.length;  move++) {
             AdvancedState cs = state.clone();
             cs.makeMove(move);
+            if (cs.hasLost()) {
+                continue;
+            }
 
-            double utility = (cs.hasLost() ? -Double.MAX_VALUE : computeUtility(state, cs, move));
-            //System.out.println("Possible utility: " + utility);
-            if (utility > bestUtility) {
+            Utility.IntDoublePair utility = (state.getHighestColumn() > 10 ?
+                                                computeUtilityWithLookAhead(state, cs)
+                                                : new Utility.IntDoublePair(0, computeUtility(state, cs, move, 0)));
+            if (utility.biggerThan(bestUtility)) {
                 bestUtility = utility;
                 bestMove = move;
             }
         }
-        System.out.println("Found best move: " + legalMoves[bestMove][0] + " - " + legalMoves[bestMove][1]);
-        AdvancedState cs = state.clone();
-        cs.makeMove(bestMove);
-        computeUtility(state, cs, bestMove);
+        System.out.println("Found best move: " + legalMoves[bestMove][0] + " - " + legalMoves[bestMove][1] + bestUtility.second);
         return bestMove;
 	}
 	
@@ -82,11 +108,10 @@ public class TestWeights {
         Scanner sc = new Scanner(System.in);
 		while(!s.hasLost()) {
 			s.makeMove(pickMove(s,s.legalMoves()));
-			s.draw();
-			s.drawNext(0,0);
+            s.draw();
+            s.drawNext(0,0);
 			try {
-                sc.nextLine();
-				Thread.sleep(10);
+				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
